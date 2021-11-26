e867ca4c327ee9710f795def4daa4327791cb050
wanglan <809382327@qq.com>
2021-11-26 16:35:15 +0800

    添加svg图标集

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -1,19 +1,30 @@
 <template>
-  <home-page />
+  <div>
+    <home-page />
+    <svg-icon class="svg-class" :svgType="'all'"/>
+  </div>
 </template>
 
 <script>
 import HomePage from '@/components/homePage.vue'
+import SvgIcon from '@/components/svgIcon.vue'
 
 export default {
   name: 'App',
-  components: { HomePage }
+  components: { HomePage,SvgIcon }
 }
 </script>
 
-<style>
+<style lang="scss">
 html {
   width: 400px;
   height: 400px;
+}
+.svg-class{
+    width: 100%;
+    height: 100%;
+    svg{
+      width: 50px;
+    }
 }
 </style>
