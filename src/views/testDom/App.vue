21d3024bba441c041c86b0afda3854074b26b90e
mouren <1832819123@qq.com>
2021-12-01 17:21:31 +0800

    main添加弹窗

--- src/views/testDom/App.vue
+++ src/views/testDom/App.vue
@@ -1,76 +1,79 @@
 <template>
-  <div class="ATTADreamMaker-dom">
-    <div class="MaskNetworkbox">
-      <div class="MaskNetwork" @click="ATTAMakerVisible = true;test()">
-         <el-dropdown trigger="click">
-           <div class="flex">
-            <svg-icon :svgType="'attaLogo'" class="svg-img"/>
-            <p class="wordtitle">ATTA Dream Maker</p>
-           </div>
-          <el-dropdown-menu slot="dropdown">
+  <div>
+    <div class="ATTADreamMaker-dom">
+      <div class="MaskNetworkbox">
+        <div class="MaskNetwork" @click="ATTAMakerVisible = true; test()">
+          <el-dropdown trigger="click">
+            <div class="flex">
+              <svg-icon :svgType="'attaLogo'" class="svg-img" />
+              <p class="wordtitle">ATTA Dream Maker</p>
+            </div>
+            <el-dropdown-menu slot="dropdown">
               <el-dropdown-item>Create NFT</el-dropdown-item>
               <el-dropdown-item>Create poster NFT</el-dropdown-item>
               <el-dropdown-item>Create Co-NFT</el-dropdown-item>
               <el-dropdown-item>NFT Hall</el-dropdown-item>
               <el-dropdown-item>My NFTs</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+      </div>
+      <div class="ConnectWalletbox">
+        <div class="ConnectWallet flex" @click="ConnectWalletloading = true">
+          <svg-icon :svgType="'attaTextLogo'" class="svg-img" />
+          <p class="wordtitle">Connect Wallet</p>
+        </div>
+        <!-- 选择钱包插件 -->
+        <el-dialog
+          title="连接钱包"
+          :visible.sync="ConnectWalletVisible"
+          width="30%"
+          :modal="false"
+          custom-class="MaskNetwork-dialogbox"
+          top="40vh"
+        >
+          <div class="walletbox" @click="checkwallclick(1)">
+            <svg-icon :svgType="'mateMask'" class="svg-img" />
+            <p>MetaMask</p>
+          </div>
+          <div class="walletbox" @click="checkwallclick(2)">
+            <svg-icon :svgType="'mateMask'" class="svg-img" />
+            <p>MetaMask</p>
+          </div>
+        </el-dialog>
+        <el-dialog
+          :title="loadingwallettitle"
+          :visible.sync="ConnectWalletloading"
+          width="30%"
+          :modal="false"
+          top="40vh"
+          custom-class="MaskNetwork-dialogbox2"
+        >
+          <div class="useraddressbox flex">
+            <svg-icon :svgType="'mateMask'" class="svg-img" />
+            <div class="contentbox" v-if="checkwallet <= 0">
+              <div class="contentbox-top flex">
+                <p>MetaMask</p>
+                <img src alt />
+              </div>
+              <div class="contentbox-bottom flex">
+                <p>address地址</p>
+                <img src alt />
+                <img src alt />
+              </div>
+            </div>
+            <div class="content-loading" v-else>
+              <p class="loading-p">{{ loadingwallettitle }}</p>
+              <div class="loading-div">Initializing…</div>
+            </div>
+            <el-button v-if="checkwallet == 0" class="editbtn" @click="userwalletdialog()">更改</el-button>
+            <el-button v-else class="editbtn" @click="userwalletdialog()">Retry</el-button>
+          </div>
+        </el-dialog>
       </div>
     </div>
-    <div class="ConnectWalletbox">
-      <div class="ConnectWallet flex" @click="ConnectWalletloading = true">
-        <svg-icon :svgType="'attaTextLogo'" class="svg-img"/>
-        <p class="wordtitle">Connect Wallet</p>
-      </div>
-      <!-- 选择钱包插件 -->
-      <el-dialog
-        title="连接钱包"
-        :visible.sync="ConnectWalletVisible"
-        width="30%"
-        :modal="false"
-        custom-class="MaskNetwork-dialogbox"
-        top="40vh"
-      >
-        <div class="walletbox" @click="checkwallclick(1)">
-          <svg-icon :svgType="'mateMask'" class="svg-img"/>
-          <p>MetaMask</p>
-        </div>
-        <div class="walletbox" @click="checkwallclick(2)">
-          <svg-icon :svgType="'mateMask'" class="svg-img"/>
-          <p>MetaMask</p>
-        </div>
-      </el-dialog>
-      <el-dialog
-        :title="loadingwallettitle"
-        :visible.sync="ConnectWalletloading"
-        width="30%"
-        :modal="false"
-        top="40vh"
-        custom-class="MaskNetwork-dialogbox2"
-      >
-        <div class="useraddressbox flex">
-          <svg-icon :svgType="'mateMask'" class="svg-img"/>
-          <div class="contentbox" v-if="checkwallet <= 0">
-            <div class="contentbox-top flex">
-              <p>MetaMask</p>
-              <img src="" alt="">
-            </div>
-            <div class="contentbox-bottom flex">
-              <p>address地址</p>
-              <img src="" alt="">
-              <img src="" alt="">
-            </div>
-          </div>
-          <div class="content-loading" v-else>
-            <p class="loading-p">{{loadingwallettitle}}</p>
-            <div class="loading-div">
-              Initializing…
-            </div>
-          </div>
-          <el-button v-if="checkwallet == 0" class="editbtn" @click="userwalletdialog()">更改</el-button>
-          <el-button v-else class="editbtn" @click="userwalletdialog()">Retry</el-button>
-        </div>
-      </el-dialog>
+    <div class="modalDom">
+      <create-nft></create-nft>
     </div>
   </div>
 </template>
@@ -80,35 +83,38 @@ import '../../../public/js/web3/web3-provider.min.js'
 import '../../../public/js/web3/chainSetting.js'
 import '../../../public/js/web3/chainProvider.js'
 import SvgIcon from '@/components/svgIcon.vue'
+import CreateNft from './createNft.vue'
 export default {
-  components: { SvgIcon },
+  components: { SvgIcon, CreateNft },
   data() {
     return {
       ATTAMakerVisible: false,//ATTA Dream Maker弹框控制
       ConnectWalletVisible: false,//选择钱包插件弹框控制
       ConnectWalletloading: false, // 钱包账户弹框控制
-      loadingwallettitle : '账户',//钱包账户标题/选中钱包插件loading标题
-      checkwallet : 0,//选择的钱包插件
+      loadingwallettitle: '账户',//钱包账户标题/选中钱包插件loading标题
+      checkwallet: 0,//选择的钱包插件
     };
   },
   mounted() {
-    console.log(localStorage.getItem("_grecaptcha"),'测试缓存');
+    console.log(localStorage.getItem("_grecaptcha"), '测试缓存');
     let leftdom = document.querySelector('[aria-label][role="navigation"]');
     let crdom = document.querySelector(".ATTADreamMaker-dom");
-    console.log(leftdom);
     leftdom.appendChild(crdom);
+    let maindom = document.querySelector('[aria-label][role="main"]');
+    const modals = document.querySelector('.modalDom')
+    maindom.appendChild(modals)
   },
   methods: {
-    test(){
+    test() {
 
-    console.log(window.CHAIN,'测试数据');
+      console.log(window.CHAIN, '测试数据');
     },
-    userwalletdialog(){
+    userwalletdialog() {
       this.ConnectWalletloading = false;
       this.ConnectWalletVisible = true;
     },
     // 选中钱包插件
-    checkwallclick(type){
+    checkwallclick(type) {
       this.ConnectWalletVisible = false;
       this.ConnectWalletloading = true;
       this.loadingwallettitle = 'Connect to MetaMask';
@@ -119,23 +125,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.flex{
+.flex {
   display: flex;
   align-items: center;
 }
-.MaskNetworkbox,.ConnectWalletbox{
+.MaskNetworkbox,
+.ConnectWalletbox {
   padding: 6px 14px;
   position: relative;
   transition-property: background-color, box-shadow;
   // transition-duration: 0.2s;
-  .MaskNetwork,.ConnectWallet{
+  .MaskNetwork,
+  .ConnectWallet {
     font-weight: 400;
     font-size: 1rem;
     cursor: pointer;
-    .svg-img{
+    .svg-img {
       flex-shrink: 0;
     }
-    .wordtitle{
+    .wordtitle {
       margin: 4px 16px 4px 20px;
       font-family: inherit;
       font-weight: 400;
@@ -144,29 +152,30 @@ export default {
     }
   }
 }
-.MaskNetworkbox:hover,.ConnectWalletbox:hover{
+.MaskNetworkbox:hover,
+.ConnectWalletbox:hover {
   background: rgba(15, 20, 25, 0.1);
   border-radius: 9999px;
 }
-.MaskNetwork-dialogbox{
+.MaskNetwork-dialogbox {
   padding: 32px 36px 16px;
-  .walletbox{
+  .walletbox {
     text-align: center;
     height: 151px;
     width: 165px;
     padding: 32px 8px;
-    background-color: #F7F9FA;
+    background-color: #f7f9fa;
     border-radius: 15px;
     display: inline-block;
     box-sizing: border-box;
     margin-right: 20px;
     cursor: pointer;
-    .svg-img{
+    .svg-img {
       width: 45px;
       height: 45px;
       margin: 0 auto;
     }
-    p{
+    p {
       font-size: 16px;
       font-weight: normal;
       white-space: nowrap;
@@ -174,47 +183,50 @@ export default {
     }
   }
 }
-.MaskNetwork-dialogbox2{
+.MaskNetwork-dialogbox2 {
   padding: 16px 32px 24px;
-  .useraddressbox{
+  .useraddressbox {
     padding: 16px 24px;
     margin-bottom: 16px;
-    background-color: #F7F9FA;
+    background-color: #f7f9fa;
     border-radius: 8px;
     box-sizing: border-box;
     position: relative;
-    .svg-img{
+    .svg-img {
       width: 48px;
       height: 48px;
     }
-    .contentbox,.content-loading{
+    .contentbox,
+    .content-loading {
       font-size: 16px;
       margin-left: 8px;
-      .contentbox-top,.contentbox-bottom{
-        p{
+      .contentbox-top,
+      .contentbox-bottom {
+        p {
           margin: 0;
           margin-right: 6px;
           font-weight: 400;
         }
-        img{
+        img {
           width: 16px;
           height: 16px;
           border: 1px solid black;
         }
       }
-      .loading-p,.loading-div{
+      .loading-p,
+      .loading-div {
         margin: 0;
         margin-right: 6px;
         font-weight: 400;
       }
     }
-    .editbtn{
+    .editbtn {
       position: absolute;
       right: 24px;
       top: 0;
       bottom: 0;
       margin: auto;
-      background-color: rgb(26,139,216);
+      background-color: rgb(26, 139, 216);
       min-width: 64px;
       height: 30px;
       padding: 4px 10px;
@@ -225,7 +237,8 @@ export default {
 }
 </style>
 <style lang="css">
-  .MaskNetwork-dialogbox .el-dialog__header,.MaskNetwork-dialogbox2 .el-dialog__header{
-    border-bottom: 1px solid #919ca3;
-  }
+.MaskNetwork-dialogbox .el-dialog__header,
+.MaskNetwork-dialogbox2 .el-dialog__header {
+  border-bottom: 1px solid #919ca3;
+}
 </style>
---
+++ src/views/testDom/createNft.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="create-nft-wrap">
+    <el-dialog
+        title="loadingwallettitle"
+        width="30%"
+        :visible="true"
+        :modal="false"
+        top="40vh"
+        custom-class="MaskNetwork-dialogbox2"
+      >
+        <el-upload
+          class="avatar-uploader"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload">
+          <img v-if="imageUrl" :src="imageUrl" class="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+      </el-dialog>
+  </div>
+</template>
+<script>
+import '../../../public/js/web3/web3.min.js'
+import '../../../public/js/web3/web3-provider.min.js'
+import '../../../public/js/web3/chainSetting.js'
+import '../../../public/js/web3/chainProvider.js'
+import SvgIcon from '@/components/svgIcon.vue'
+export default {
+  components: { SvgIcon },
+  data() {
+    return {
+      imageUrl: ''
+    };
+  },
+  mounted(){
+
+  },
+
+  methods: {
+    handleAvatarSuccess(res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw);
+      },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg';
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!');
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!');
+      }
+      return isJPG && isLt2M;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.create-nft-wrap {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  width: 500px;
+  height: 300px;
+  z-index: 9999;
+  background: rgba($color: #000000, $alpha: 0.8);
+  transform: translate(-40%,-50%);
+}
+.avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+
+</style>
+<style lang="css">
+  .MaskNetwork-dialogbox .el-dialog__header,.MaskNetwork-dialogbox2 .el-dialog__header{
+    border-bottom: 1px solid #919ca3;
+  }
+</style>
