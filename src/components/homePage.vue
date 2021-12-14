51c2de82a355f4f6dfbe4cd13f895b581bc88fb2
zhanghaiqing <zhanghaiqing>
2021-12-14 14:19:23 +0800

    12.14-6

--- src/components/homePage.vue
+++ src/components/homePage.vue
@@ -6,6 +6,7 @@
     <div v-else>
       <a target="_blank" href="https://twitter.com">twitter</a>
     </div>
+    <button @click="topay">支付</button>
   </div>
 </template>
 
@@ -58,6 +59,37 @@ export default {
         });
 
       console.log('跳转', this.$route);
+    },
+    topay(){
+      debugger
+      console.log(window.Web3);
+      var web3 = new window.Web3(window.CHAIN.WALLET.provider());
+      window.CHAIN.WALLET.chainId()
+      .then(function (res) {
+        let {tokenId,metadataIpfs,returnaddress} = event.detail;
+        metadataIpfs = 'https://ipfs.io/ipfs/' + metadataIpfs
+        let chainId = web3.utils.hexToNumber(res); 
+        let setting_proof = chainSetting["contractSetting"]["dreammaker_minfnft"];
+        let address = setting_proof[chainId].address; // 监听 网络切换 会 让 用户 处于 正确的网络，这里 只负责 配置 当前网络下正确的 合约地址
+        var abiName = contractSetting['dreammaker_minfnft']['abi'];
+        
+        paymentexample = new web3.eth.Contract(abiName, address);  
+        
+        paymentexample.methods.safeMint(tokenId,returnaddress,metadataIpfs).send({ 
+          from: returnaddress
+        })
+        .then(function (res) {
+          debugger
+          // tips(self.chEnTextHtml[self.lang].tipsjs4);
+          // self.getNftLists();
+        });
+        // setTimeout(() => {
+        // 	tips(self.chEnTextHtml[self.lang].tipsjs5);
+        // 	setTimeout(function(){
+        // 		cancelMobile();
+        // 	},1800);
+        // }, 1000);
+      });
     }
   }
 }
