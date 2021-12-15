a4f65e8f52e07731ff820d48e37dc7bd97848f71
zhanghaiqing <zhanghaiqing>
2021-12-15 11:50:10 +0800

    修改字段

--- src/views/twitter/components/dreamMaker/dreamMaker.vue
+++ src/views/twitter/components/dreamMaker/dreamMaker.vue
@@ -18,7 +18,7 @@
       <div class="nft-list">
         <div v-for="(item,index) in nftlist" :key="index" class="dream-maker-nft">
           <!-- xxx为我创建的nft 提示条 item.mintUser 可以获取到用户id，但是不能获取到name -->
-          <span v-if="item.creator_tag == 1">User xxx created for you</span>
+          <span v-if="item.creatorTag == 1">User xxx created for you</span>
           <img :src="base_url + JSON.parse(item.nftContent).picturePath " alt="">
           <p>Name：{{JSON.parse(item.nftContent).name}}</p>
           <p>Description：</p>
@@ -31,8 +31,8 @@
           <p v-if="item.status == 2 && item.type == 2">Status：Accept / Minting</p>
           <p v-if="item.status == 3 && item.type == 2">Status：Accept / Minted</p>
           <p>Wallet：{{item.address.substring(0, 5) + "***" +item.address.substr(item.address.length - 4)}}</p>
-          <button v-if="item.status == 0 && item.type == 2 && item.creator_tag == 1">Accept</button>
-          <button v-if="item.status == 1 && item.type == 2 && item.creator_tag == 1">Mint now</button>
+          <button v-if="item.status == 0 && item.type == 2 && item.creatorTag == 1">Accept</button>
+          <button v-if="item.status == 1 && item.type == 2 && item.creatorTag == 1">Mint now</button>
         </div>
       </div>
     </div>
