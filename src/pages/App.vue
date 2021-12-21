abb8fc795082917b8b8e0546613aed41800a2736
mouren <1832819123@qq.com>
2021-12-21 18:13:07 +0800

    Update App.vue

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -11,14 +11,7 @@ import SvgIcon from '@/components/svgIcon.vue'
 
 export default {
   name: 'App',
-  components: { HomePage,SvgIcon },
-  mounted() {
-    var oMeta = document.createElement('meta');
-    console.log(oMeta,'----');
-        oMeta.content = "default-src 'self' data: gap: https://ssl.gstatic.com 'unsafe-eval'; style-src 'self' 'unsafe-inline'; media-src self https://plugin.bazhuayu.io;script-src 'self' http://onlineerp.solution.quebec 'unsafe-inline' 'unsafe-eval';";
-        oMeta['http-equiv'] = 'Content-Security-Policy';
-        document.getElementsByTagName('head')[0].appendChild(oMeta);
-  },
+  components: { HomePage,SvgIcon }
 }
 </script>
 
